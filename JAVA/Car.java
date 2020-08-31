1a1f23dda83b63b2deffda490b357f4980c126da
enrique garcia <enriquegarcia259@yahoo.es>
2020-08-30 18:31:21 -0600

    poli

--- JAVA/Car.java
+++ JAVA/Car.java
@@ -1,20 +1,44 @@
+import java.util.concurrent.Flow.Publisher;
+
 class Car {
 
     // atributos
     Integer id;
     String license;
     Account driver; // porque hereda de la clase Account
-    Integer passegenger;
+    private Integer passegenger;
 
     // constructor
     public Car(String license, Account driver) {
         this.license = license;
         this.driver = driver;
+
     }
 
     // metodo para imprimir las propiedades del objeto
     void printDataCar() {
+        if (passegenger != null) {
+            System.out.println("License: " + license + "Diver: " + driver.name + "Passengers: " + passegenger);
+
+        }
         // imprimo del conductor
-        System.out.println("License: " + license + "Diver: " + driver.name);
     }
+
+    // get
+    public Integer getPassenger() {
+        return passegenger;
+    }
+
+    // permiten alterar este dato dentro de la clase
+    public void setPassenger(Integer passenger) {
+        // manejar la cantidad de pasajeros(validar)
+        if (passenger == 4) {
+            this.passegenger = passenger;
+
+        } else {
+            System.out.println("Necesitas asignar 4 pasajeros");
+        }
+
+    }
+
 }
